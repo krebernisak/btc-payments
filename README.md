fc992bf229d367d26d56a879365ec466e0557714
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-01-21 14:26:02 +0100

    Setup production environment using Gunicorn & NGINX with docker compose

--- README.md
+++ README.md
@@ -8,22 +8,35 @@ Implement a Python REST web service that provides the functionality to create BT
 
 Install dependencies into your environment by running the following in the terminal:
 
+```bash
+$ pip install -r ./btc_api/requirements/dev.txt
 ```
-$ pip install -r requirements.txt
-```
+
+This will install dependencies for development environment including black formatter and flake8 linter (excluded in prod).
 
 ### Run development server
 
-Start Flask development server by running the following in the terminal:
+Start Flask development server (in debug mode) by running the following in the terminal:
 
-```
-$ env FLASK_APP=app.py flask run
+```bash
+$ cd btc_api/src
+$ env FLASK_APP=app.py FLASK_DEBUG=1 flask run
 ```
 
 ### Install the hooks
 
 Install the git hooks defined in the pre-commit file by running the following in the terminal:
 
+```bash
+$ pre-commit install
 ```
-# pre-commit install
+
+## Production deployment
+
+The production environment scales Python Flask App using [Gunicorn](https://gunicorn.org/) application server and [NGINX](https://www.nginx.com/) web server using multiple Containers with Docker Compose.
+
+### Docker Compose
+
+```bash
+$ docker-compose up --build --detach
 ```
--- btc_api/src/app.py
+++ btc_api/src/app.py
@@ -12,20 +12,20 @@ def hello():
 @app.route("/payment_transactions")
 def payment_transactions():
     """
-    This endpoint will be used to create a raw transaction that spends from a P2PKH 
+    This endpoint will be used to create a raw transaction that spends from a P2PKH
     address and that supports paying to multiple addresses (either P2PKH or P2SH).
-    
+
     The endpoint should return a transaction that spends from the source address and
     that pays to the output addresses. An extra output for change should be included
     in the resulting transaction if the change is > 5430 SAT.
-    
+
     URL: /payment_transactions
     Method: POST
     Request body (dictionary):
         source_address (string): The address to spend from
         outputs (dictionary): A dictionary that maps addresses to amounts (in SAT)
         fee_kb (int): The fee per kb in SAT
-    
+
     Response body (dictionary):
         raw (string): The unsigned raw transaction
         inputs (array of dicts): The inputs used
@@ -36,3 +36,17 @@ def payment_transactions():
     """
     name = request.args.get("name", "BTC")
     return f"Hello, {escape(name)}!"
+
+
+def main():
+    use_debugger = app.debug
+    app.run(
+        use_debugger=use_debugger,
+        debug=app.debug,
+        use_reloader=use_debugger,
+        host="0.0.0.0",
+    )
+
+
+if __name__ == "__main__":
+    main()
